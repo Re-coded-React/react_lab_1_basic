379662e8c3abadc8f53cf84a38486f3491bf5eca
ZETTA <faeza.smohammed@gmail.com>
2021-01-25 20:03:10 +0300

    Post added

---
+++ src/components/Post.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Post extends Component {
+    render() {
+        return (
+            <div>
+                Its my first post
+            </div>
+        );
+    }
+}
+
+export default Post;
