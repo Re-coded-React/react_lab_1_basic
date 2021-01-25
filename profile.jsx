dcf1cf82fee4801153c196118dc804076ed3f95c
unknown <ismail.th.khalil@gmail.com>
2021-01-25 19:48:36 +0300

    ismail changes

---
+++ profile.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class profile extends Component {
+    render() {
+        return (
+            <div>
+                <img href='google.com' alt='an image for an employee'></img>
+                <title>Civil Engineer</title>
+                <p>work in construction sites</p>
+                <link href='https://www.facebook.com/ismail.thanoon.7'>Facebook account</link>
+            </div>
+        )
+    }
+}
