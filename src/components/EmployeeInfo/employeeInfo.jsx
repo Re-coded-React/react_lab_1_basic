59ace2c91b9503c61b9b2acc73f1d2781ebd5da5
Abdulazeez <abdulazeezalabbasi@yahoo.com>
2021-01-25 19:46:53 +0300

    componentes added

---
+++ src/components/EmployeeInfo/employeeInfo.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class employeeInfo extends Component {
+    render() {
+        return (
+            <div>
+                <p>Employee's name</p>
+                <p>Employee's job</p>
+            </div>
+        )
+    }
+}
---
+++ src/components/profile/profile.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class profile extends Component {
+    render() {
+        return (
+            <div>
+                <img src="" alt=""/>
+                <h1>Title</h1>
+                <p>job description</p>
+                <a href="">social media</a>
+            </div>
+        )
+    }
+}
